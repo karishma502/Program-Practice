0289481809da96265fa519da53c1f1a34435d3f6
Karishma <karishmakate31@gmail.com>
2024-09-07 00:25:13 +0530

    #Leetcode problems 2024

--- src/test/java/PracticeCode_e/DeleteNodesFromLinkedListPresentinArray.java
+++ src/test/java/PracticeCode_e/DeleteNodesFromLinkedListPresentinArray.java
@@ -6,7 +6,23 @@ public class DeleteNodesFromLinkedListPresentinArray {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		  ListNode head = new ListNode(1);
+	        head.next = new ListNode(2);
+	        head.next.next = new ListNode(3);
+	        head.next.next.next = new ListNode(4);
+	        head.next.next.next.next = new ListNode(5);
 
+	        // Array of values to be removed: [1, 2, 3]
+	        int[] nums = {1, 2, 3};
+
+	        System.out.println("Original list:");
+	        printList(head);
+
+	        // Calling the modifiedList method
+	        ListNode modifiedHead = modifiedList(nums, head);
+
+	        System.out.println("Modified list after removal:");
+	        printList(modifiedHead);
 	}
 
 	public static ListNode modifiedList(int[] nums, ListNode head) {
@@ -32,5 +48,16 @@ public class DeleteNodesFromLinkedListPresentinArray {
 		current.next = null;
 		return temp.next;
 	}
+	
+    // Method to print the linked list
+    public static void printList(ListNode head) {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.val + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
 
 }
