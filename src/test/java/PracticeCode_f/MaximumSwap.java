9b17bef793e5592941d0298c3a872541c1f799cd
Karishma <karishmakate31@gmail.com>
2024-10-17 23:47:37 +0530

    #Leetcode problems 2024

---
+++ src/test/java/PracticeCode_f/MaximumSwap.java
@@ -0,0 +1,46 @@
+package PracticeCode_f;
+/*
+ * TC-O(n)
+ * SC-O(1)
+ */
+public class MaximumSwap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n1=2736;
+		System.out.println(maximumSwap(n1));
+		int n2=9973;
+		System.out.println(maximumSwap(n2));
+	
+	}
+	public static int maximumSwap(int num) {
+		// Convert number to string for digit manipulation
+		char[] numArr = Integer.toString(num).toCharArray();
+		int n = numArr.length;
+
+		// Track the last occurrence of each digit (0-9)
+		int[] last = new int[10];
+		for (int i = 0; i < n; i++) {
+			last[numArr[i] - '0'] = i;
+		}
+
+		// Traverse the number from left to right
+		for (int i = 0; i < n; i++) {
+			// Check if we can find a larger digit to swap
+			for (int d = 9; d > numArr[i] - '0'; d--) {
+				if (last[d] > i) {
+					// Swap and return the new number
+					char temp = numArr[i];
+					numArr[i] = numArr[last[d]];
+					numArr[last[d]] = temp;
+					return Integer.parseInt(new String(numArr));
+				}
+			}
+		}
+
+		// Return the original number if no swap occurred
+		return num;
+	}
+
+
+}
