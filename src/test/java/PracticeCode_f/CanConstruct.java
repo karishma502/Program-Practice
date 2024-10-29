2e3c6beee1c208b807364566290405ad0b4a1326
Karishma <karishmakate31@gmail.com>
2024-10-29 23:52:27 +0530

    #Leetcode problems 2024

---
+++ src/test/java/PracticeCode_f/CanConstruct.java
@@ -0,0 +1,21 @@
+package PracticeCode_f;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CanConstruct {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+    public boolean canConstruct(String ransomNote, String magazine) {
+
+        Map<String,String> map= new HashMap<>();
+        for(int i=0;i<magazine.length();i++){
+        }
+		return false;
+        
+    }
+
+}
