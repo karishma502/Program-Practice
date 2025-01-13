88c4d9648ce6336119e86193e5bde4a476f2342c
Karishma <karishmakate31@gmail.com>
2025-01-13 23:33:16 +0530

    Program Practice.

---
+++ src/test/java/PracticeCode_h/MinimumLength.java
@@ -0,0 +1,35 @@
+package PracticeCode_h;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MinimumLength {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Test case 1
+		String[] words1 = "abaacbcbb";
+		System.out.println(wordSubsets(words1, words2));
+
+	}
+	public static  List<String> wordSubsets(String[] words1, String[] words2) {
+		public int minimumLength(String s) {
+			int[] charFrequency = new int[26];
+			int totalLength = 0;
+			for (char c : s.toCharArray()) {
+				charFrequency[c - 'a']++;
+			}
+			for (int frequency : charFrequency) {
+				if (frequency == 0) continue;
+				if (frequency % 2 == 0) {
+					totalLength += 2;
+				} else {
+					totalLength += 1;
+				}
+			}
+			return totalLength;
+		}
+
+
+	}
