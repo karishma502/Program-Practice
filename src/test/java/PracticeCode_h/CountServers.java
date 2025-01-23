e51c05f8f65ed0b9b8976166da4d4da92bd16f1f
Karishma <karishmakate31@gmail.com>
2025-01-23 14:05:59 +0530

    Program Practice.

---
+++ src/test/java/PracticeCode_h/CountServers.java
@@ -0,0 +1,32 @@
+package PracticeCode_h;
+
+public class CountServers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[][] grid ={{1,0},{0,1}};
+		System.out.println(countServers(grid));
+	}
+	public static int countServers(int[][] grid) {
+		int[] Rows = new int[grid.length];
+		int[] Col = new int[grid[0].length];
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[0].length; j++) {
+				Rows[i] += grid[i][j];
+				Col[j] += grid[i][j];
+			}
+		}
+		int ans = 0;
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[0].length; j++) {
+				if (grid[i][j] == 1 && (Rows[i] > 1 || Col[j] > 1)) {
+					ans++;
+				}
+			}
+		}
+		return ans;
+	}
+
+
+}
