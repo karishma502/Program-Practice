93292bb35b42753bd980766ba7c6feabc2c040ba
Karishma <karishmakate31@gmail.com>
2025-02-01 16:02:05 +0530

    Program Practice.

---
+++ src/test/java/PracticeCode_i/ArraySpecial.java
@@ -0,0 +1,19 @@
+package PracticeCode_i;
+
+public class ArraySpecial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] n = {1};
+		System.out.println(isArraySpecial(n));
+	}
+	public static boolean isArraySpecial(int[] nums) {
+		for (int i = 0; i < nums.length - 1; i++) {
+			if (nums[i] % 2 == nums[i + 1] % 2) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+}
